c58d7d5c2cac0bd74a8f778c2b856a5078654569
Dushyant Mathur <dushyantmathur08@gmail.com>
2024-09-05 02:59:57 +0530

    learn about strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,40 @@
+
+const name ="dushyant"
+const repoCount = 50
+
+//console.log(name + repoCount+ " Value")
+
+///console.log(`hello my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String('dushyant-fc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(5));
+//console.log(gameName.indexOf('s'));
+
+//const newString = gameName.substring(0,4);
+//console.log(newString);
+
+// const anotherString = gameName.slice(-8,4);
+
+// console.log(anotherString);
+
+// const newStringOne = "   dushyant    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://dushyant.com/dushyant%20mathur"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('dushyant'));
+
+console.log(gameName.split('-'));
+
+
+
